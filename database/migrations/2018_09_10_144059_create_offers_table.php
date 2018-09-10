2f39c965a7f49f894d931db64496c4e0d19b520b
Alexander Zavyalov <whmordred@gmail.com>
2018-09-10 22:49:29 +0800

    DB structure

---
+++ database/migrations/2018_09_10_144059_create_offers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->double('price');
+            $table->unsignedInteger('amount')->default(0);
+            $table->unsignedInteger('sales')->default(0);
+            $table->string('article');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
