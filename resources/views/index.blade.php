b3126ba63aa34aed4ef764787b5a8d8e444b5de4
Alexander Zavyalov <whmordred@gmail.com>
2018-09-11 11:35:03 +0800

    Simple page template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="content">
+    <div class="flex-1 sidenav">
+        <b>Категории</b>
+    </div>
+
+    <div class="flex-3">
+        <h1>20 самых популярных товаров</h1>
+    </div>
+</div>
+@endsection
